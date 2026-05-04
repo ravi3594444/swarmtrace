5ce3779426f2c8535985536fa2abfe7e5a970674
ravi3594444 <ravi3594444@users.noreply.github.com>
2026-05-04 07:29:47 +0000

    fix README - remove duplicates

--- tracely/__init__.py
+++ tracely/__init__.py
@@ -1,5 +1,5 @@
 from tracely.tracer import observe
 from tracely.storage import get_traces, save_trace
 
-__version__ = "0.1.2"
-__all__ = ["observe", "get_traces", "save_trace"]
+__version__ = '0.1.4'
+__all__ = ['observe', 'get_traces', 'save_trace']
