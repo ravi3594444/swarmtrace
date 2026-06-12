a3bbc183fb33f190705b0b8dc6eed18ee4912ace
ravi3594444 <ravi3594444@users.noreply.github.com>
2026-06-12 10:40:46 +0000

    release 0.2.0: budget, tool_attention, scraper, replay, show_failures

--- tracely/__init__.py
+++ tracely/__init__.py
@@ -1,5 +1,14 @@
 from tracely.tracer import observe, init
 from tracely.storage import get_traces, save_trace
+from tracely.budget import budget, reset as reset_budget, get_usage
+from tracely.replay import show_failures
+from tracely.tool_attention import ToolAttention
 
-__version__ = '0.1.9'
-__all__ = ['observe', 'init', 'get_traces', 'save_trace']
+__version__ = '0.2.0'
+__all__ = [
+    'observe', 'init',
+    'get_traces', 'save_trace',
+    'budget', 'reset_budget', 'get_usage',
+    'show_failures',
+    'ToolAttention',
+]
